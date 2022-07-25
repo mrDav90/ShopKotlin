aea4f7bb565ba834f3bd2226964ff887afb8e4c9
mrDav90 <kangnidavidgabiam@esp.sn>
2022-07-25 21:19:18 +0000

    new updates

--- app/src/main/java/com/example/shopappkotlin/MainActivity.kt
+++ app/src/main/java/com/example/shopappkotlin/MainActivity.kt
@@ -3,18 +3,21 @@ package com.example.shopappkotlin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val startBtn = findViewById<Button>(R.id.startBtn);
-        startBtn.setOnClickListener {
+        Handler().postDelayed({
             intent = Intent(this , Connexion::class.java);
             startActivity(intent)
-        }
+        }, 5000)
+
 
     }
 }
